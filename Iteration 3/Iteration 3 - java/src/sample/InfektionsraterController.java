622500f5d79626979db342ef4d48d222ba29a11d
MariaSilke <79915330+MariaSilke@users.noreply.github.com>
2021-06-02 17:26:00 +0200

    Add files via upload

---
+++ Iteration 3/Iteration 3 - java/src/sample/InfektionsraterController.java
@@ -0,0 +1,42 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class InfektionsraterController {
+    public Text LabelKommune;
+    public TextField TextKommune;
+    public Button ButtonSøg;
+    public Text Dato;
+    public DatePicker DatePicker;
+    public Text labelPostnummer;
+    public TextField textPostnummer;
+    public Text labelSmittede;
+    public TextField textSmittede;
+    public BarChart barchartMutationer;
+
+    @FXML
+    private GridPane grid;
+
+    private void clearTextFields(GridPane grid) {
+        for (Node node : grid.getChildren()) {
+            if (node instanceof TextField) {
+                // clear
+                ((TextField) node).setText("");
+                node.setStyle(null);
+            }
+        }
+    }
+
+    public void ActionSearch(ActionEvent actionEvent) { clearTextFields(grid);
+    }
+}
+
---
+++ Iteration 3/Iteration 3 - java/src/sample/SmitteoversigtController.java
@@ -0,0 +1,28 @@
+package sample;
+
+
+import javafx.scene.control.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+
+public class SmitteoversigtController {
+
+    public Text labelUdvikling;
+    public Text labelDøde;
+    public Text labelIndlagte;
+    public Text labelIntensiv;
+    public Text labelRespirator;
+    public Text labelPrøveAntal;
+    public Text labelPositivProcent;
+    public Text labelRaskmeldte;
+    public Text labelOpdateret;
+    public Text labelSmittede;
+
+}
