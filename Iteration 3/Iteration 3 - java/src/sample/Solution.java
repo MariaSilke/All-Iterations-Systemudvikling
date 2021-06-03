631b7d11ba014d00c41a166a06b94ad7d0136b7b
MariaSilke <79915330+MariaSilke@users.noreply.github.com>
2021-06-03 23:25:07 +0200

    Update Solution.java

--- Iteration 3/Iteration 3 - java/src/sample/Solution.java
+++ Iteration 3/Iteration 3 - java/src/sample/Solution.java
@@ -1,40 +1,33 @@
 package sample;
 import jakarta.xml.bind.JAXBContext;
-import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 import java.io.File;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        try {
-            // create an instance of `JAXBContext`
-            JAXBContext context = JAXBContext.newInstance(Summary.class);
-
-            // create an instance of `Marshaller`
-            Marshaller marshaller = context.createMarshaller();
-
-            // enable pretty-print XML output
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-
-            // write XML to `StringWriter`
-            File file = new File("summary.xml");
-
-            // create `Book` object
-            Summary summary = new Summary("1507651124", "2750", "Positiv", "20210101", "B.1.1.7", "B.1.1.7");
-
-            // convert book object to XML
-            marshaller.marshal(summary, file);
-
-            // print the XML
-            System.out.println(file.toString());
-
-        } catch (JAXBException ex) {
-            ex.printStackTrace();
-        }
-
-        }
-
+        Summary summary1 = XMLtoSummaryExample("summary1.xml");
+        System.out.println(person);
+        summary1ToXMLExample("summary1-output.xml", summary1);
     }
 
+    private static Summary XMLtoSummaryExample(String filename) throws Exception {
+        File file = new File(filename);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Summary.class);
 
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        return (Summary) jaxbUnmarshaller.unmarshal(file);
+    }
+
+    private static void summary1ToXMLExample(String filename, Summary summary1) throws Exception {
+        File file = new File(filename);
+        JAXBContext jaxbContext = JAXBContext.newInstance(Summary.class);
+
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+
+        jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
+        jaxbMarshaller.marshal(summary1, file);
+        jaxbMarshaller.marshal(summary1, System.out);
+    }
+}
